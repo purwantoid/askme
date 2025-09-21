f6f29e53ed6e8e76d17101f5e768c31117956857
purwantoid <purwanto@outlook.com>
2025-09-21 20:48:41 +0700

    feat/roles - reorganize api, web controller

--- app/Http/Controllers/Web/RoleController.php
+++ app/Http/Controllers/Web/RoleController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Web;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Role\StoreRoleRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\RoleController;
+use App\Http\Controllers\Web\ProfileController;
+use App\Http\Controllers\Web\RoleController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
